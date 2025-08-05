efd5d6e8f4b94950f3cb0420138b35901106a2ac
祐寧 <113306006@g.nccu.edu.tw>
2025-08-05 15:46:00 +0800

    幹

--- room.js
+++ room.js
@@ -22,48 +22,82 @@ let gamePlaying = false;
 
 // "圖片介面"點擊事件
 function cardImg(e) {
+    const img = e.target;
+    const parentCard = img.closest('.card-item');
     if (!gamePlaying) {
-        const img = e.target;
-        const parentCard = e.target.closest('.card-item');
+
         const editFold = parentCard.querySelector('.card-edit-fold');
 
         img.style.display = 'none';
         editFold.style.display = 'flex';
+    } else {
+
+        const gusesFold = parentCard.querySelector('.card-guess-fold');
+
+        img.style.display = 'none';
+        gusesFold.style.display = 'flex';
     }
-}
-
-// "編輯蓋牌介面"點擊事件
-function cardEditFold(e) {
-    editFold = e.target;
-    const parentCard = editFold.closest('.card-item');
-    const img = parentCard.querySelector('.card-img');
-
-    editFold.style.display = 'none';
-    img.style.display = 'flex';
-
 }
 
 // "蓋牌介面"點擊事件
 function cardFolded(e) {
-    folded = e.target;
-    const parentCard = folded.closest('.card-item');
+    const interface = e.target;
+    const parentCard = interface.closest('.card-item');
     const img = parentCard.querySelector('.card-img');
 
-    folded.style.display = 'none';
+    interface.style.display = 'none';
     img.style.display = 'flex';
 
 }
 
+// "編輯蓋牌介面"點擊事件
+function cardEditFold(e) {
+    const interface = e.target;
+    const parentCard = interface.closest('.card-item');
+    const img = parentCard.querySelector('.card-img');
+
+    interface.style.display = 'none';
+    img.style.display = 'flex';
+
+}
+
+// "猜測蓋牌介面"點擊事件
+function cardGuessFold(e) {
+    const interface = e.target;
+    const parentCard = interface.closest('.card-item');
+    const img = parentCard.querySelector('.card-img');
+
+    interface.style.display = 'none';
+    img.style.display = 'flex';
+
+}
+
+// "改圖改名介面"點擊事件
+function cardImgName(e) {
+    const interface = e.target;
+    interface.style.display = 'none';
+}
 
 
 
 
 
+
+
+//編輯圖片
 function edit(e) {
     e.stopPropagation();
+    const target = e.target;
+    // const parentElement = target.parentElement;
+    const parentCard = target.closest('.card-item');
+    const imgName = parentCard.querySelector('.card-img-name');
+
+    // parentElement.style.display = "none";
+    imgName.style.display = "flex";
     console.log("編輯圖片");
 }
 
+//蓋牌
 function fold(e) {
     e.stopPropagation();
     const target = e.target;
@@ -76,3 +110,31 @@ function fold(e) {
     console.log("蓋上圖片");
 }
 
+//猜目標
+function guess(e) {
+    e.stopPropagation();
+    console.log("猜目標");
+}
+
+//改圖
+function img(e) {
+    e.stopPropagation();
+    console.log("改圖");
+}
+
+//改名
+function name(e) {
+    e.stopPropagation();
+    console.log("改名");
+}
+
+
+
+
+
+// 改變遊戲狀態
+function changeState(btn) {
+    gamePlaying = !gamePlaying;
+    btn.textContent = gamePlaying ? "進行中" : "停止中";
+    console.log("遊戲狀態切換為：", gamePlaying ? "進行中" : "停止中");
+}
