647dd44ebdb665d6ed9637f307b0dd03041e2e8b
祐寧 <113306006@g.nccu.edu.tw>
2025-08-05 19:48:38 +0800

    666

--- room.js
+++ room.js
@@ -18,6 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 let gamePlaying = false;
+let myTurn = true;
 
 
 // "圖片介面"點擊事件
@@ -26,10 +27,10 @@ function cardImg(e) {
     const parentCard = img.closest('.card-item');
     if (!gamePlaying) {
 
-        const editFold = parentCard.querySelector('.card-edit-fold');
+        const imgName = parentCard.querySelector('.card-img-name');
 
         img.style.display = 'none';
-        editFold.style.display = 'flex';
+        imgName.style.display = 'flex';
     } else {
 
         const gusesFold = parentCard.querySelector('.card-guess-fold');
@@ -51,15 +52,15 @@ function cardFolded(e) {
 }
 
 // "編輯蓋牌介面"點擊事件
-function cardEditFold(e) {
-    const interface = e.target;
-    const parentCard = interface.closest('.card-item');
-    const img = parentCard.querySelector('.card-img');
+// function cardEditFold(e) {
+//     const interface = e.target;
+//     const parentCard = interface.closest('.card-item');
+//     const img = parentCard.querySelector('.card-img');
 
-    interface.style.display = 'none';
-    img.style.display = 'flex';
+//     interface.style.display = 'none';
+//     img.style.display = 'flex';
 
-}
+// }
 
 // "猜測蓋牌介面"點擊事件
 function cardGuessFold(e) {
@@ -75,7 +76,11 @@ function cardGuessFold(e) {
 // "改圖改名介面"點擊事件
 function cardImgName(e) {
     const interface = e.target;
+    const parentCard = interface.closest('.card-item');
+    const img = parentCard.querySelector('.card-img');
+
     interface.style.display = 'none';
+    img.style.display = 'flex';
 }
 
 
@@ -84,17 +89,17 @@ function cardImgName(e) {
 
 
 
-//編輯圖片
+//編輯資訊
 function edit(e) {
     e.stopPropagation();
     const target = e.target;
-    // const parentElement = target.parentElement;
+    const parentElement = target.parentElement;
     const parentCard = target.closest('.card-item');
     const imgName = parentCard.querySelector('.card-img-name');
 
-    // parentElement.style.display = "none";
+    parentElement.style.display = "none";
     imgName.style.display = "flex";
-    console.log("編輯圖片");
+    console.log("編輯資訊");
 }
 
 //蓋牌
@@ -113,17 +118,23 @@ function fold(e) {
 //猜目標
 function guess(e) {
     e.stopPropagation();
-    console.log("猜目標");
+    if (myTurn) {
+        console.log("猜目標");
+    }else{
+        alert("現在是敵方回合不能猜目標");
+        console.log("不能猜目標");
+    }
+
 }
 
 //改圖
-function img(e) {
+function uploadImg(e) {
     e.stopPropagation();
     console.log("改圖");
 }
 
 //改名
-function name(e) {
+function changeName(e) {
     e.stopPropagation();
     console.log("改名");
 }
@@ -137,4 +148,11 @@ function changeState(btn) {
     gamePlaying = !gamePlaying;
     btn.textContent = gamePlaying ? "進行中" : "停止中";
     console.log("遊戲狀態切換為：", gamePlaying ? "進行中" : "停止中");
+}
+
+// 改變回合
+function changeTurn(btn) {
+    myTurn = !myTurn;
+    btn.textContent = myTurn ? "我的回合" : "敵方回合";
+    console.log("回合切換為：", myTurn ? "我的回合" : "敵方回合");
 }
