4324dc29fb4fb142d818ed8cac2cba12c9c520f6
祐寧 <113306006@g.nccu.edu.tw>
2025-08-05 15:02:31 +0800

    哈

--- room.js
+++ room.js
@@ -16,3 +16,63 @@ document.addEventListener('DOMContentLoaded', () => {
     setRoomIdFromURL();
 });
 
+
+let gamePlaying = false;
+
+
+// "圖片介面"點擊事件
+function cardImg(e) {
+    if (!gamePlaying) {
+        const img = e.target;
+        const parentCard = e.target.closest('.card-item');
+        const editFold = parentCard.querySelector('.card-edit-fold');
+
+        img.style.display = 'none';
+        editFold.style.display = 'flex';
+    }
+}
+
+// "編輯蓋牌介面"點擊事件
+function cardEditFold(e) {
+    editFold = e.target;
+    const parentCard = editFold.closest('.card-item');
+    const img = parentCard.querySelector('.card-img');
+
+    editFold.style.display = 'none';
+    img.style.display = 'flex';
+
+}
+
+// "蓋牌介面"點擊事件
+function cardFolded(e) {
+    folded = e.target;
+    const parentCard = folded.closest('.card-item');
+    const img = parentCard.querySelector('.card-img');
+
+    folded.style.display = 'none';
+    img.style.display = 'flex';
+
+}
+
+
+
+
+
+
+function edit(e) {
+    e.stopPropagation();
+    console.log("編輯圖片");
+}
+
+function fold(e) {
+    e.stopPropagation();
+    const target = e.target;
+    const parentElement = target.parentElement;
+    const parentCard = target.closest('.card-item');
+    const folded = parentCard.querySelector('.card-folded');
+
+    parentElement.style.display = "none";
+    folded.style.display = "flex";
+    console.log("蓋上圖片");
+}
+
